f8ff2ef6efd54c67e4a3fb4faebbdc0a043a1c1c
Moritz Fuller <fullermoritz@gmail.com>
2020-06-10 12:25:06 +0200

    CRC is now uppercase

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 ID=$(xxd -u -p canisters/game_of_life/_canister.id)
-CRC=$(python2 -c "import crc8;h=crc8.crc8();h.update('$ID'.decode('hex'));print(h.hexdigest())")
+CRC=$(python2 -c "import crc8;h=crc8.crc8();h.update('$ID'.decode('hex'));print(h.hexdigest().upper())")
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 xdg-open "http://127.0.0.1:8000/?canisterId=ic:$ID$CRC"
