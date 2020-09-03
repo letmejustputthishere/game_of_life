6d5829e136f926dd24ed1c4dde4614da69c6498f
Moritz Fuller <fullermoritz@gmail.com>
2020-09-03 10:59:56 +0200

    updated to 0.6.4

--- start.sh
+++
@@ -1,8 +0,0 @@
-ID=$(xxd -u -p canisters/game_of_life/_canister.id)
-CRC=$(python2 -c "import crc8;h=crc8.crc8();h.update('$ID'.decode('hex'));print(h.hexdigest().upper())")
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-xdg-open "http://127.0.0.1:8000/?canisterId=ic:$ID$CRC"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-open "http://127.0.0.1:8000/?canisterId=ic:$ID$CRC"
-fi
